b6a002f501fe6ea53a73665cf95b34f536a6d0d0
Pintututun <priyabratasatapathy00@gmail.com>
2024-04-04 23:13:50 +0530

    13th commit

---
+++ SpringBasics_FieldInjectionOfTwoPredefined/src/com/nt/sbeans/WishMessageGenerator.java
@@ -0,0 +1,40 @@
+package com.nt.sbeans;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component("wmg")
+
+public class WishMessageGenerator {
+//HAS-A-Property
+	@Autowired
+	private LocalDateTime dateTime;
+	@Autowired
+	private Date date;
+	
+	
+
+		//Business method
+	public String generateWishMessage(String user) {
+		System.out.println("WishMessageGenerator.generateWishMessage()");
+		//Get current hour of the day
+		System.out.println(dateTime+"  "+date);
+		int hour=dateTime.getHour();
+		
+		//Generate the wish message
+		if(hour<12)
+			return "Good Morning:"+user;
+		else if(hour<16)
+			return "Good Afternoon:"+user;
+		else if(hour<20)
+		    return "Good Evening:"+user;
+		else
+			return "Good Night:"+user;
+		
+	}
+	
+	
+}
