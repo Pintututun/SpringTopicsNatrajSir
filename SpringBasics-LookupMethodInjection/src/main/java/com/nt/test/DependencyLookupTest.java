c836c8fa2bd9dc539ed91c1160fbcf219f92e1d1
Pintututun <priyabratasatapathy00@gmail.com>
2024-04-04 23:30:19 +0530

    28th commit

---
+++ SpringBasics-LookupMethodInjection/src/main/java/com/nt/test/DependencyLookupTest.java
@@ -0,0 +1,21 @@
+package com.nt.test;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.nt.sbeans.Cricketer;
+
+public class DependencyLookupTest {
+
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext ctx=new ClassPathXmlApplicationContext("com/nt/cfgs/applicationContext.xml");
+		
+		Cricketer cktr=ctx.getBean("cktr",Cricketer.class);
+		
+		cktr.batting();
+		cktr.bowling();
+		cktr.fielding();
+         
+		ctx.close();
+	}
+
+}
